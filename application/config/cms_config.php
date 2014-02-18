645bfeca23ad3c609285d1a302ed9ea29c042ab4
dushyantmk <dushyantkanungo@yahoo.com>
2014-02-18 07:06:24 +0000

    Ben's the boss

--- application/config/cms_config.php
+++ application/config/cms_config.php
@@ -1,6 +1,6 @@
 <?php
 /*Written by Dushyant Kanungo on 03 Feb 2014*/
 
-$config['site_name'] = 'Dadu CMS Framework'
+$config['site_name'] = 'Dee\'s Tees'
 
 ?>
--- application/controllers/page.php
+++ application/controllers/page.php
@@ -12,6 +12,10 @@
 			// Fetch all the page data
 			$this->data['page'] = $this->page_m->get_by(array('slug' => (string)$this->uri->segment(1)), TRUE);
 			count($this->data['page']) || show_404(current_url());
+			
+			// Fetch the data
+			
+			// Load the view
 			$this->load->view('_main_layout', $this->data);
 			
     	}
